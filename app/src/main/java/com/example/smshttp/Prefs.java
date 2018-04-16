4c75213b06309f0fbd502c714b63ee37d10177db
donquixote2u <donquixote2u@gmail.com>
2018-04-16 19:32:59 +1200

    Initial commit

---
+++ app/src/main/java/com/example/smshttp/Prefs.java
@@ -0,0 +1,31 @@
+package com.example.smshttp;
+
+import com.example.smshttp.R;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.util.Log;
+
+
+public class Prefs extends PreferenceActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		addPreferencesFromResource(R.xml.prefs);
+	}
+	
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+	    Preference pref = findPreference(key);
+
+	    if (pref instanceof EditTextPreference) {
+	    	EditTextPreference textPref = (EditTextPreference) pref;
+	        pref.setSummary(textPref.getSummary());
+	        Log.d("KALSMS", "textPref.getSummary(): " + textPref.getSummary());
+	    }
+	}
+}
+
